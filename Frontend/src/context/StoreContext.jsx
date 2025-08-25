57b33af9bf3dba135e9fc0310507ce0f726d5381
Saxenaishita4321 <ishitasaxena261@gmail.com>
2025-08-25 23:34:37 +0530

    Updated package.json and StoreContext

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
    const [cartItems, setCartItems] = useState({});
-   const url = "http://localhost:4000";
+   // const url = "http://localhost:4000";
+   const API_URL = process.env.REACT_APP_API_URL;
    const [token, setToken] = useState("");
    const [food_list, setFoodList] = useState([]);
 
