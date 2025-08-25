1948acb345598a664f4294b2cbe5745769e2d1e0
Saxenaishita4321 <ishitasaxena261@gmail.com>
2025-08-26 00:51:40 +0530

    Fixed StoreContext to use REACT_APP_API_URL

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
    const [cartItems, setCartItems] = useState({});
    // const url = "http://localhost:4000";
-   const API_URL = process.env.REACT_APP_API_URL;
+      const API_URL = import.meta.env.VITE_API_URL; 
    const [token, setToken] = useState("");
    const [food_list, setFoodList] = useState([]);
 
